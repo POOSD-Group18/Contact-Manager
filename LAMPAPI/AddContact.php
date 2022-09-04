9669f468c7b278ff38bc78ec11e1961bc676f4b9
Kyle Mason <kylemason@knights.ucf.edu>
2022-09-04 15:11:39 -0400

    Complete add, search, and login. Useful for reference if the front end wants to look at the format of the input.

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,18 +1,23 @@
 <?php
 	$inData = getRequestInfo();
 	
-	$color = $inData["color"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$email = $inData["email"];
+	$phoneNumber = $inData["phoneNumber"];
+	$address = $inData["address"];
 	$userId = $inData["userId"];
+	$name = $inData["firstName"] . " " . $inData["lastName"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331G18");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $color);
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Email, PhoneNumber, Address, UserID, Name) VALUES (?,?,?,?,?,?,?)");
+		$stmt->bind_param("sssssss", $firstName, $lastName, $email, $phoneNumber, $address, $userId, $name);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
