4275be7498ad1cd2e9bc7f250fd38ee2c98282ac
JosefVodicka <josef.vodicka@yahoo.com>
2022-09-13 12:29:19 -0400

    scriptjs

--- js/script.js
+++ js/script.js
@@ -15,7 +15,51 @@ async function doLogin()
     let login = document.getElementById("loginName").value;
     let password = document.getElementById("loginPassword").value;
 
-    const payload = {username:login,password:password}
+    document.getElementById("loginResult").innerHTML = "";
 
-    const res = await axios.post(urlBase + '/Login' + extension)
+
+    try
+    {
+        const payload = {username:login,password:password}
+
+        const res = await axios.post(urlBase + '/Login' + extension)
+
+        if(res.data.error != ""){
+            throw new Error(res.data.error)
+        }
+
+        else{
+            const userId = res.data.id
+            saveCookie("userId", userId)
+            window.location.href = "manager.html";
+        }
+    }
+
+    catch(e){
+        document.getElementById("loginResult").innerHTML = 'Invalid username or password.'
+        console.log("Invalid username or password")
+        
+    }
+    
+}
+
+
+function doLogout()
+{
+    userId = 0;
+    firstName = "";
+    lastname = "";
+
+    document.cookie = "firstName = ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.href = "main.html";
+}
+
+function saveCookie(cookieName, id)
+{
+    let minutes = 10;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+    const expires = "expires=" + date.toUTCString();
+    document.cookie =  cookieName + "=" + id + "; " + expires + "; path=/";
+
 }
